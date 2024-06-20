a9024aa107ac90412f199474eb7b4b46106d54d3
Alex Xiong <alex.xiong.tech@gmail.com>
2024-06-20 11:50:12 +0800

    add foo.cpp for dev test

---
+++ fplll/foo.cpp
@@ -0,0 +1,12 @@
+// #include "test.h"
+
+// TODO: add some test functions
+#include <string>
+#include "foo.h"
+
+FPLLL_BEGIN_NAMESPACE
+std::string greet(std::string name) {
+    return std::string("hello, ") + name;
+}
+
+FPLLL_END_NAMESPACE
---
+++ fplll/foo.h
@@ -0,0 +1,13 @@
+#ifndef FPLLL_FOO_H
+#define FPLLL_FOO_H
+
+#include <string>
+#include "defs.h"
+
+FPLLL_BEGIN_NAMESPACE
+
+std::string greet(std::string);
+
+FPLLL_END_NAMESPACE
+
+#endif // FPLLL_FOO_H
